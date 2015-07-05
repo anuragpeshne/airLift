76359e461d177c018d5f9a0de398dea1c5475600
anuragpeshne <anurag.peshne@gmail.com>
2015-07-05 17:29:02 +0530

    more robust, smarter: skip is exists

--- lift2.py
+++ lift2.py
@@ -2,6 +2,7 @@ import urllib2
 import re
 import os
 from os import listdir
+import os.path
 from os.path import expanduser
 import sys
 
@@ -14,19 +15,34 @@ def lift(currentLocation):
         if isDirRe.search(anchor):
             lift(currentLocation + anchor)
         elif(targetFileRe.search(anchor)):
-            fileReq = urllib2.Request(sourceLocation+currentLocation+anchor)
-            if not os.path.exists(unloadLocation + urllib2.unquote(currentLocation)):
+            fileReq = urllib2.Request(sourceLocation + currentLocation + anchor)
+            if not os.path.exists(unloadLocation +
+                    urllib2.unquote(currentLocation)):
                 os.makedirs(unloadLocation + urllib2.unquote(currentLocation))
-            local_file = open(unloadLocation + urllib2.unquote(currentLocation) + anchor, 'wb')
-            local_file.write(urllib2.urlopen(fileReq).read())
-            local_file.close()
-            print(unloadLocation + urllib2.unquote(currentLocation))
+            if not os.path.isfile(unloadLocation +
+                    urllib2.unquote(currentLocation + anchor)):
+                local_file = open(unloadLocation +
+                        urllib2.unquote(currentLocation + anchor), 'wb')
+                local_file.write(urllib2.urlopen(fileReq).read())
+                local_file.close()
+                print(unloadLocation + urllib2.unquote(currentLocation))
+            else:
+                print(urllib2.unquote(anchor) + ' exists, skipping...')
 
+if len(sys.argv) < 2:
+    print("airLift: enter remote location and port number\n" +
+            "Example: python lift.py http://192.168.0.1:8000")
+    sys.exit(0) #fatal
+sourceLocation = sys.argv[1]
+if len(sys.argv) > 2:
+    targetFileTypes = sys.argv[2:]
+else:
+    targetFileTypes = ['.txt','.pdf']
+    print('Note: No file extension specified')
+print('Will copy ' + ','.join(targetFileTypes))
 userHome = expanduser("~")
 unloadLocation = userHome + '/Downloads'
-print('unloading stuff at '+unloadLocation)
-targetFileTypes = ['.txt','.djvu']
-sourceLocation = sys.argv[1]
+print('unloading stuff at ' + unloadLocation)
 anchorRe = re.compile('<a href=[\'"](.*)[\'"]>', re.I)
 isDirRe = re.compile('/$')
 targetFileRe = re.compile('(' + '|'.join(targetFileTypes)+')$', re.I)
--- lift3.py
+++ lift3.py
@@ -3,6 +3,7 @@ import urllib.request
 import re
 import os
 from os import listdir
+import os.path
 from os.path import expanduser
 import sys
 
@@ -15,19 +16,45 @@ def lift(currentLocation):
         if isDirRe.search(anchor):
             lift(currentLocation + anchor)
         elif(targetFileRe.search(anchor)):
-            fileReq = urllib.request.Request(sourceLocation+currentLocation+anchor)
-            if not os.path.exists(unloadLocation + urllib.parse.unquote(currentLocation)):
-                os.makedirs(unloadLocation + urllib.parse.unquote(currentLocation))
-            local_file = open(unloadLocation + urllib.parse.unquote(currentLocation) + anchor, 'wb')
-            local_file.write(urllib.request.urlopen(fileReq).read())
-            local_file.close()
-            print(unloadLocation + urllib.parse.unquote(currentLocation))
+            fileReq = urllib.request.Request(sourceLocation +
+                    currentLocation + anchor)
+            if not os.path.exists(
+                unloadLocation + urllib.parse.unquote(currentLocation)
+            ):
+                os.makedirs(
+                    unloadLocation + urllib.parse.unquote(currentLocation)
+                )
+            if not os.path.isfile(
+                unloadLocation +
+                urllib.parse.unquote(currentLocation) +
+                urllib.parse.unquote(anchor)
+            ):
+                local_file = open(
+                    unloadLocation +
+                    urllib.parse.unquote(currentLocation) +
+                    urllib.parse.unquote(anchor),
+                    'wb'
+                )
+                local_file.write(urllib.request.urlopen(fileReq).read())
+                local_file.close()
+                print(unloadLocation + urllib.parse.unquote(currentLocation))
+            else:
+                print(urllib.parse.unquote(anchor) + ' exists, skipping...')
 
+if len(sys.argv) < 2:
+    print("airLift: enter remote location and port number\n" +
+            "Example: python lift.py http://192.168.0.1:8000")
+    sys.exit(0) #fatal
+sourceLocation = sys.argv[1]
+if len(sys.argv) > 2:
+    targetFileTypes = sys.argv[2:]
+else:
+    targetFileTypes = ['.txt','.pdf']
+    print('Note: No file extension specified')
+print('Will copy ' + ','.join(targetFileTypes))
 userHome = expanduser("~")
 unloadLocation = userHome + '/Downloads'
 print('unloading stuff at '+unloadLocation)
-targetFileTypes = ['.pdf','.djvu']
-sourceLocation = sys.argv[1]
 anchorRe = re.compile('<a href=[\'"](.*)[\'"]>', re.I)
 isDirRe = re.compile('/$')
 targetFileRe = re.compile('(' + '|'.join(targetFileTypes)+')$', re.I)
